b6f56c3f3b283b1b7f4c70ffccd9fecb5a6f1178
Kishan <kishanrehkadia79@gmail.com>
2024-03-19 19:09:02 +0530

    adding persistent storage

--- GenAiParser/PdfParserLLAMA.py
+++ GenAiParser/PdfParserLLAMA.py
@@ -1,5 +1,10 @@
 from llama_index.llms.openai import OpenAI
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage
+)
 from IPython.display import Markdown, display
 
 documents = SimpleDirectoryReader("Docs").load_data()
@@ -10,7 +15,13 @@ for doc in documents:
     print(doc.text)
 
 index = VectorStoreIndex.from_documents(documents)
+index.storage_context.persist()
+
+'''
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
+index = load_index_from_storage(storage_context=storage_context)
 query_engine = index.as_query_engine()
 response = query_engine.query("What is the total experience?")
 print(response)
 display(Markdown(f"<b>{response}</b>"))
+'''
--- GenAiParser/PdfParserPyPdf.py
+++ GenAiParser/PdfParserPyPdf.py
@@ -1,10 +1,10 @@
 import PyPDF2
-filename = "D:\Code\Machine-Learning\GithubRepo\machine-learning-and-GenAI-tutorial\GenAiParser\multiplication-chart.pdf"
+filename = "D:\Code\Machine-Learning\GithubRepo\machine-learning-and-GenAI-tutorial\GenAiParser\Docs\Samsung_Invoice.pdf"
 pdf_file = open(filename, 'rb')
 
 reader = PyPDF2.PdfReader(pdf_file)
 
-page_length = 1
+page_length = 2
 page_num = 1
 for i in range(page_length-1):
     page = reader.pages[i]
